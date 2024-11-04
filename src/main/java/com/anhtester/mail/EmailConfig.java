ca69a2c759126a68c7311d0aab0f322041af53d7
fasiash <fasiuddin@codetru.com>
2024-11-04 10:09:02 +0530

    added email

--- src/main/java/com/anhtester/mail/EmailConfig.java
+++ src/main/java/com/anhtester/mail/EmailConfig.java
@@ -21,9 +21,9 @@ public class EmailConfig {
     public static final String SERVER = "smtp.gmail.com";
     public static final String PORT = "587";
 
-    public static final String FROM = "anhtester@email.com";
+    public static final String FROM = "codetruteamlead@email.com";
     public static final String PASSWORD = "******";
 
-    public static final String[] TO = {"anhtester@mailinator.com"};
+    public static final String[] TO = {"fasiuddin@codetru.com"};
     public static final String SUBJECT = REPORT_TITLE;
 }
